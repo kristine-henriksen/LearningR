368dad7b0d3485b1236891533cd7723d1a14b697
Kristine Henriksen <kristinehenriksen@live.dk>
2021-06-16 14:07:56 +0200

    More exercises

--- R/exercises-wrangling.R
+++ R/exercises-wrangling.R
@@ -188,7 +188,7 @@ nhanes_modified
 
 
 
-# 9.14 --------------------------------------------------------------------
+# 9.14-15 --------------------------------------------------------------------
 
 # summary statistics by group
 
@@ -210,4 +210,51 @@ summary(nhanes_small)
 nhanes_small %>%
     summarise(max_bmi = max(bmi, na.rm = TRUE),
               min_bmi = min(bmi, na.rm = TRUE))
+
+
+
+# Exercise 9.16 -----------------------------------------------------------
+
+# 1. Calculate the mean of weight and age
+nhanes_small %>%
+    summarise(mean_weight = mean(weight, na.rm = TRUE),
+              mean_age = mean(age, na.rm =TRUE))
+
+# 2. Calculate the max and min of height
+nhanes_small %>%
+    summarise(max_height = max(height, na.rm = TRUE),
+              min_height = min(height, na.rm = TRUE))
+
+# 3. Calculate the median of age and phys_active_days
+nhanes_small %>%
+    summarise(median_age = median(age, na.rm = TRUE),
+              median_phys_active_days = median(phys_active_days, na.rm = TRUE))
+
+
+
+# 9.17 + 9.19 --------------------------------------------------------------------
+
+# Summary statistics by a group
+nhanes_small %>%
+    group_by(diabetes) %>%
+    summarise(mean_age = mean(age, na.rm = TRUE),
+              mean_bmi = mean(bmi, na.rm = TRUE))
+
+nhanes_small %>%
+    filter(!is.na(diabetes)) %>%
+    group_by(diabetes) %>%
+    summarise(mean_age = mean(age, na.rm = TRUE),
+              mean_bmi = mean(bmi, na.rm = TRUE)) %>%
+    ungroup()
+
+
+## SAVING DATASETS/FRAMES AS FILES
+
+#saving data as an .rda file in the data folder
+usethis::use_data(nhanes_small,
+                  overwrite = TRUE)
+
+
+
+
 
